cc2c8b56361f86508c9ef425cabcb80b82617375
Nico Burniske <nicoburniske@gmail.com>
2025-08-29 18:38:27 +0000

    working flake???

--- flake.nix
+++ flake.nix
@@ -3,11 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    apple-silicon = {
+      url = "github:nix-community/nixos-apple-silicon";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, apple-silicon }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
+      specialArgs = { inherit apple-silicon; };
       modules = [ ./configuration.nix ];
     };
   };
