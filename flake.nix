c3352158f0a609a362298c93d32f0f230b1a7ac4
Nico Burniske <nicoburniske@gmail.com>
2025-08-29 17:27:13 +0000

    Convert to flake-based configuration with home-manager

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS configuration with home-manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.nico = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
