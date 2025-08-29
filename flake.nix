9da90b5a676ff45cede5781de8dc83d89fe234ef
Nico Burniske <nicoburniske@gmail.com>
2025-08-29 19:04:26 +0000

    add home-manager v0

--- flake.nix
+++ flake.nix
@@ -7,13 +7,25 @@
       url = "github:nix-community/nixos-apple-silicon";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, apple-silicon }: {
+  outputs = { self, nixpkgs, apple-silicon, home-manager }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       specialArgs = { inherit apple-silicon; };
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nico = import ./home.nix;
+        }
+      ];
     };
   };
 }
