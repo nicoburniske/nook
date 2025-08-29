9f98ab98ae1e8588c9b972de67f6ffd00d049a35
Nico Burniske <nicoburniske@gmail.com>
2025-08-29 19:41:29 +0000

    hyprland config

--- flake.nix
+++ flake.nix
@@ -27,6 +27,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
           home-manager.users.nico = import ./home.nix;
           home-manager.sharedModules = [ stylix.homeManagerModules.stylix ];
         }
