a2c060564e1e267c611f4d3d75d1d58cfb99f4a1
Nico Burniske <nicoburniske@gmail.com>
2025-08-30 13:42:14 +0000

    custom swaylock config file

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@ in {
     ./helix.nix
     ./waybar.nix
     ./starship.nix
+    ./swaylock.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should manage.
   home.username = "nico";
@@ -426,23 +427,7 @@ in {
   };
 
 
-  programs.swaylock.package = pkgs.swaylock-effects;
-  programs.swaylock = {
-    enable = true;
 
-    settings = {
-      font-size = 24;
-      show-failed-attempts = true;
-      indicator = true;
-
-      effect-blur="7x5";
-      effect-vignette="0.5:0.5";
-
-      clock = true;
-      datestr="%a, %B %e";
-      timestr="%I:%M %p";
-    };
-  };
 
     programs.ghostty = {
     enable = true;
