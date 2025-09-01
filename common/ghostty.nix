e7e8297e8fd0e309aea3e91f55d85701201ac1af
Nico Burniske <nicoburniske@gmail.com>
2025-09-01 10:48:41 -0400

    wip mac stuff

--- common/ghostty.nix
+++ common/ghostty.nix
@@ -1,6 +1,8 @@
-{ ... }: {
+{ config, lib, pkgs, ... }: {
   programs.ghostty = {
     enable = true;
+
+    package = lib.mkIf pkgs.stdenv.isDarwin null;
 
     settings = {
       shell-integration-features = "no-cursor,no-title";
@@ -8,12 +10,17 @@
       gtk-single-instance = true;
       adjust-cursor-thickness = 5;
 
+      window-title-font-family = config.stylix.fonts.monospace.name;
+
       window-theme = "auto";
       window-decoration = "none";
       window-padding-balance = true;
       window-padding-x = 5;
       window-padding-y = 0;
       window-padding-color = "extend";
+
+      macos-titlebar-style = "hidden";
+      macos-option-as-alt = true;
 
       scrollback-limit = 104857600;
 
