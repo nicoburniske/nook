10544df60f5af1f8702c3995ebeb6ff642a9f552
Nico Burniske <nicoburniske@gmail.com>
2025-09-02 20:35:10 -0400

    just use auto-theming for delta

--- common/git.nix
+++ common/git.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }: {
+{ ... }: {
   programs.git = {
     enable = true;
     userName = "Nico Burniske";
@@ -34,64 +34,6 @@
         side-by-side = true;
         line-numbers = true;
         hyperlinks = true;
-        features = "stylix";
-
-        "stylix" = with config.lib.stylix.colors; {
-          "${
-            if config.stylix.polarity == "dark"
-            then "dark"
-            else "light"
-          }" =
-            true;
-
-          file-style = "#${base05} bold";
-          file-decoration-style = "#${base0A} ul";
-          file-added-label = "[+]";
-          file-copied-label = "[==]";
-          file-modified-label = "[*]";
-          file-removed-label = "[-]";
-          file-renamed-label = "[->]";
-
-          hunk-header-decoration-style = "#${base0D} box ul";
-          hunk-header-file-style = "#${base0C}";
-          hunk-header-line-number-style = "#${base0A}";
-          hunk-header-style = "file line-number syntax";
-
-          line-numbers-left-format = "{nm:>3}┊";
-          line-numbers-right-format = "{np:>3}┊";
-          line-numbers-left-style = "#${base03}";
-          line-numbers-right-style = "#${base03}";
-          line-numbers-minus-style = "#${base08} bold";
-          line-numbers-plus-style = "#${base0B} bold";
-          line-numbers-zero-style = "#${base04}";
-
-          minus-style = "syntax #${base01}";
-          minus-emph-style =
-            "syntax "
-            + (
-              if config.stylix.polarity == "dark"
-              then "#332222"
-              else "#fff5f5"
-            );
-          plus-style = "syntax #${base01}";
-          plus-emph-style =
-            "syntax "
-            + (
-              if config.stylix.polarity == "dark"
-              then "#223322"
-              else "#f5fff5"
-            );
-
-          zero-style = "syntax";
-          whitespace-error-style = "#${base08} reverse";
-
-          commit-decoration-style = "#${base0A} box";
-          commit-style = "#${base05} bold";
-
-          blame-code-style = "syntax";
-          blame-format = "{author:<18} ({commit:>7}) ┊{timestamp:^16}┊ ";
-          blame-palette = "#${base00} #${base01} #${base02} #${base03}";
-        };
       };
     };
   };
--- common/lazygit.nix
+++ common/lazygit.nix
@@ -1,8 +1,11 @@
-{ ... }: {
+{ config, ... }: let
+  theme = if config.stylix.polarity == "light" then "--light" else "--dark";
+  in {
+
   xdg.configFile."lazygit/config.yml".text = ''
     git:
       colorArg: always
       paging:
-        pager: delta --true-color=never --paging=never --line-numbers
+        pager: delta --true-color=never --paging=never --line-numbers ${theme}
   '';
 }
