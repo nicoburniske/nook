6a3361672d3a2c349079bf4c3bc8fb6c9733da62
Nico Burniske <nicoburniske@gmail.com>
2025-09-02 20:46:58 -0400

    simplify starship config

--- common/starship.nix
+++ common/starship.nix
@@ -3,7 +3,7 @@
     enable = true;
     enableTransience = true;
     settings = {
-      format = "$directory$nix_shell$fill$git_branch$git_status$cmd_duration$line_break$character";
+      format = "$directory$nix_shell$fill$git_branch$cmd_duration$line_break$character";
       add_newline = false;
       c.disabled = true;
 
@@ -28,24 +28,8 @@
         format = "[]($style)[[ ](bg:#${base01} fg:#${base0A} bold)$branch](bg:#${base01} fg:#${base05} bold)[ ]($style)";
         style = "bg:none fg:#${base01}";
       };
-      git_status = {
-        format = "[]($style)[$all_status$ahead_behind](bg:#${base01} fg:#${base09} bold)[ ]($style)";
-        style = "bg:none fg:#${base01}";
-        conflicted = "=";
-        ahead = "[⇡\${count} ](fg:#${base0B} bg:#${base01}) ";
-        behind = "[⇣\${count} ](fg:#${base08} bg:#${base01})";
-        diverged = "↑\${ahead_count} ⇣\${behind_count} ";
-        up_to_date = "[](fg:#${base0B} bg:#${base01})";
-        untracked = "[?\${count} ](fg:#${base09} bg:#${base01}) ";
-        stashed = "";
-        modified = "[~\${count} ](fg:#${base09} bg:#${base01})";
-        staged = "[+\${count} ](fg:#${base0B} bg:#${base01}) ";
-        renamed = "[󰑕\${count} ](fg:#${base0A} bg:#${base01})";
-        deleted = "[ \${count} ](fg:#${base08} bg:#${base01}) ";
-      };
       cmd_duration = {
         min_time = 1;
-        # duration & style ;
         format = "[]($style)[[ ](bg:#${base01} fg:#${base08} bold)$duration](bg:#${base01} fg:#${base05} bold)[]($style)";
         disabled = false;
         style = "bg:none fg:#${base01}";
