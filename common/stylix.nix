5e1c9ceb972cd8c8a5a8c66582974fe95785661e
Nico Burniske <nicoburniske@gmail.com>
2025-09-05 07:26:50 -0400

    ochre

--- common/stylix.nix
+++ common/stylix.nix
@@ -72,8 +72,9 @@ in {
           scheme = "Modus Operandi Tinted";
           author = "Protesilaos Stavrou";
           base00 = "fbf7f0"; # bg-main - primary background
-          base01 = "f1d5d0"; # bg-hl-line - light pinkish highlight background
-          base02 = "f0c1cf"; # bg-completion - darker pinkish selection color
+          base01 = "f0e3c0"; # bg-ochre - light yellow selection color
+          base02 = "f1d5d0"; # bg-hl-line - light pinkish highlight background
+          # base02 = "f0e3c0"; # bg-ochre - light yellow selection color
           base03 = "595959"; # fg-dim - lighter/dimmed text
           base04 = "585858"; # fg-mode-line-inactive - darkest text
           base05 = "000000"; # fg-main - primary text
