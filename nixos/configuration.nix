e7cc474a675a0bf5b1cfdfbce1b048d75234426f
Nico Burniske <nicoburniske@gmail.com>
2025-10-03 18:21:05 -0400

    caps lock and esc are esc

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -78,11 +78,14 @@ in {
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
 
-  services.interception-tools = {
+  services.interception-tools = let
+    itools = pkgs.interception-tools;
+    itools-caps = pkgs.interception-tools-plugins.caps2esc;
+  in {
     enable = true;
-    plugins = [pkgs.interception-tools-plugins.caps2esc];
+    plugins = [itools-caps];
     udevmonConfig = ''
-      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+      - JOB: "${itools}/bin/intercept -g $DEVNODE | ${itools-caps}/bin/caps2esc -m 1 | ${itools}/bin/uinput -d $DEVNODE"
         DEVICE:
           EVENTS:
             EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
