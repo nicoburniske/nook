f3c47370ef09801738882c39a13ed5e43ca2e019
Nico Burniske <nicoburniske@gmail.com>
2025-10-19 19:54:59 -0400

    open files in existing helix instance

--- common/lazygit.nix
+++ common/lazygit.nix
@@ -1,13 +1,24 @@
-{config, ...}: let
+{config, pkgs, ...}: let
   theme =
     if config.stylix.polarity == "light"
     then "--light"
     else "--dark";
   colors = config.lib.stylix.colors;
+  
+  sendToHelix = cmd: pkgs.lib.concatStringsSep " && " [
+    "kitty @ send-text --match 'state:overlay_parent' '\\x1b'"
+    "kitty @ send-text --match 'state:overlay_parent' \"${cmd}\""
+    "kitty @ send-text --match 'state:overlay_parent' '\\r'"
+    "kitten @ close-window --match state:self"
+  ];
 in {
   programs.lazygit = {
     enable = true;
     settings = {
+      os = {
+        edit = sendToHelix ":open {{filename}}";
+        editAtLine = sendToHelix ":open {{filename}}:{{line}}";
+      };
       git = {
         colorArg = "always";
         paging = {
