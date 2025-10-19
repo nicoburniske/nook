bc211bbe963ff1821dde307ce38eb8941fdaf6be
Nico Burniske <nicoburniske@gmail.com>
2025-10-19 06:38:01 -0400

    add files channel to television

--- common/television.nix
+++ common/television.nix
@@ -14,4 +14,24 @@
     [preview]
     command = "bat -n --color=always '{}'"
   '';
+
+  xdg.configFile."television/cable/text.toml".text = ''
+    [metadata]
+    name = "text"
+    description = "A channel to find and select text from files"
+    requirements = [ "rg", "bat",]
+
+    [source]
+    command = "rg . --no-heading --line-number --colors 'match:fg:white' --colors 'path:fg:blue' --color=always"
+    ansi = true
+    output = "{strip_ansi|split:\\::..2}"
+
+    [preview]
+    command = "bat -n --color=always '{strip_ansi|split:\\::0}'"
+    offset = "{strip_ansi|split:\\::1}"
+
+    [ui.preview_panel]
+    header = "{strip_ansi|split:\\::..2}"
+
+  '';
 }
