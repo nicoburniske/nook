b0aa84eece097b479094a18ca221ee7348051e3b
Nico Burniske <nicoburniske@gmail.com>
2026-02-16 07:16:00 -0500

    helix plugin file tree

--- modules/helix-plugin/plugins/init.scm
+++ modules/helix-plugin/plugins/init.scm
@@ -1,4 +1,12 @@
 (require "splash.scm")
+(require "cogs/keymaps.scm")
+
+(define file-tree-base (deep-copy-global-keybindings))
+(merge-keybindings file-tree-base FILE-TREE-KEYBINDINGS)
+(set-global-buffer-or-extension-keymap (hash FILE-TREE file-tree-base))
+
+(keymap (global)
+        (normal (C-f ":create-file-tree")))
 
 (when (equal? (command-line) '("hx"))
   (show-splash))
