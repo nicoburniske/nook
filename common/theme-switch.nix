43162200eacfca9be900c8376e3c229affbe2728
Nico Burniske <nicoburniske@gmail.com>
2026-03-25 12:07:53 -0400

    kitty quick access terminal broken, use fuzzel for theme switch

--- common/theme-switch.nix
+++ common/theme-switch.nix
@@ -1,10 +1,40 @@
 {pkgs}:
-pkgs.writeNuScriptBin "theme-switch" ''
-  let kitten = "${pkgs.kitty}/bin/kitten"
-  let nu_bin = "${pkgs.nushell}/bin/nu"
+if pkgs.stdenv.isDarwin
+then
+  pkgs.writeNuScriptBin "theme-switch" ''
+    let kitten = "${pkgs.kitty}/bin/kitten"
+    let nu_bin = "${pkgs.nushell}/bin/nu"
 
-  ^$kitten quick-access-terminal --instance-group theme-selector $nu_bin -c '
-    let fzf = "${pkgs.fzf}/bin/fzf"
+    ^$kitten quick-access-terminal --instance-group theme-selector $nu_bin -c '
+      let fzf = "${pkgs.fzf}/bin/fzf"
+      let themes_result = (do { ^sumi facets theme } | complete)
+
+      if $themes_result.exit_code != 0 {
+        exit 0
+      }
+
+      let themes = ($themes_result.stdout | str trim)
+      if $themes == "" {
+        exit 0
+      }
+
+      let pick = (do {
+        $themes | ^$fzf --prompt "Select theme: " --layout reverse --border rounded --color dark
+      } | complete)
+
+      if $pick.exit_code != 0 {
+        exit 0
+      }
+
+      let theme = ($pick.stdout | str trim)
+      if $theme != "" {
+        ^sumi switch $"theme=($theme)"
+      }
+    '
+  ''
+else
+  pkgs.writeNuScriptBin "theme-switch" ''
+    let fuzzel = "${pkgs.fuzzel}/bin/fuzzel"
     let themes_result = (do { ^sumi facets theme } | complete)
 
     if $themes_result.exit_code != 0 {
@@ -17,7 +47,7 @@ pkgs.writeNuScriptBin "theme-switch" ''
     }
 
     let pick = (do {
-      $themes | ^$fzf --prompt "Select theme: " --layout reverse --border rounded --color dark
+      $themes | ^$fuzzel --dmenu --prompt "theme> "
     } | complete)
 
     if $pick.exit_code != 0 {
@@ -28,5 +58,4 @@ pkgs.writeNuScriptBin "theme-switch" ''
     if $theme != "" {
       ^sumi switch $"theme=($theme)"
     }
-  '
-''
+  ''
