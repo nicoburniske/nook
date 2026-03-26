6ef20cc5a3687eaf414f58eb9898df4d1b2ac9ea
Nico Burniske <nicoburniske@gmail.com>
2026-03-26 10:23:36 -0400

    theme-switch: use sumi structured output

--- common/theme-switch.nix
+++ common/theme-switch.nix
@@ -1,55 +1,24 @@
-{pkgs}:
-if pkgs.stdenv.isDarwin
-then
-  pkgs.writeNuScriptBin "theme-switch" ''
-    let kitten = "${pkgs.kitty}/bin/kitten"
-    let nu_bin = "${pkgs.nushell}/bin/nu"
-
-    ^$kitten quick-access-terminal --instance-group theme-selector $nu_bin -c '
-      let fzf = "${pkgs.fzf}/bin/fzf"
-      let themes_result = (do { ^sumi facets theme } | complete)
-
-      if $themes_result.exit_code != 0 {
-        exit 0
-      }
-
-      let themes = ($themes_result.stdout | str trim)
-      if $themes == "" {
-        exit 0
-      }
-
-      let pick = (do {
-        $themes | ^$fzf --prompt "Select theme: " --layout reverse --border rounded --color dark
-      } | complete)
-
-      if $pick.exit_code != 0 {
-        exit 0
-      }
-
-      let theme = ($pick.stdout | str trim)
-      if $theme != "" {
-        ^sumi switch $"theme=($theme)"
-      }
-    '
-  ''
-else
-  pkgs.writeNuScriptBin "theme-switch" ''
-    let fuzzel = "${pkgs.fuzzel}/bin/fuzzel"
-    let themes_result = (do { ^sumi facets theme } | complete)
+{pkgs}: let
+  loadThemes = ''
+    let themes_result = (do { ^sumi facets theme --json } | complete)
 
     if $themes_result.exit_code != 0 {
       exit 0
     }
 
-    let themes = ($themes_result.stdout | str trim)
+    let themes = (
+      $themes_result.stdout
+      | from json
+      | get variants
+      | str join "\n"
+      | str trim
+    )
     if $themes == "" {
       exit 0
     }
+  '';
 
-    let pick = (do {
-      $themes | ^$fuzzel --dmenu --prompt "theme> "
-    } | complete)
-
+  applySelection = ''
     if $pick.exit_code != 0 {
       exit 0
     }
@@ -58,4 +27,33 @@ else
     if $theme != "" {
       ^sumi switch $"theme=($theme)"
     }
+  '';
+in
+if pkgs.stdenv.isDarwin
+then
+  pkgs.writeNuScriptBin "theme-switch" ''
+    let kitten = "${pkgs.kitty}/bin/kitten"
+    let nu_bin = "${pkgs.nushell}/bin/nu"
+
+    ^$kitten quick-access-terminal --instance-group theme-selector $nu_bin -c '
+      let fzf = "${pkgs.fzf}/bin/fzf"
+      ${loadThemes}
+
+      let pick = (do {
+        $themes | ^$fzf --prompt "Select theme: " --layout reverse --border rounded --color dark
+      } | complete)
+
+      ${applySelection}
+    '
+  ''
+else
+  pkgs.writeNuScriptBin "theme-switch" ''
+    let fuzzel = "${pkgs.fuzzel}/bin/fuzzel"
+    ${loadThemes}
+
+    let pick = (do {
+      $themes | ^$fuzzel --dmenu --prompt "theme> "
+    } | complete)
+
+    ${applySelection}
   ''
