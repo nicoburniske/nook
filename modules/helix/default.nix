9f061e11777cefbd64868c2bba5cceaaa5c2d714
Nico Burniske <nicoburniske@gmail.com>
2026-04-14 11:43:50 -0400

    theme oos symlink

--- modules/helix/default.nix
+++ modules/helix/default.nix
@@ -5,6 +5,7 @@
     ...
   }: let
     tomlFormat = pkgs.formats.toml {};
+    helixRoot = "${config.lib.sumi.paths.flakeRootOrErr}/modules/helix";
 
     helixSteelPackage =
       (inputs.helix-steel.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
@@ -31,133 +32,139 @@
       pkgs.nixd
     ];
 
-    sumi.configFile = {
+    sumi.configFile =
+      {
       "helix/config.toml" = {
         watch = ["theme"];
         value = ctx: let
           theme = ctx.values.theme;
-        in
-          tomlFormat.generate "sumi-helix-config-${ctx.selection.theme}.toml" {
-            theme = theme.meta.helix or ctx.selection.theme;
+          in
+            tomlFormat.generate "sumi-helix-config-${ctx.selection.theme}.toml" {
+              theme = theme.meta.helix or ctx.selection.theme;
 
-            editor = {
-              bufferline = "always";
-              cursorline = true;
-              color-modes = true;
-              true-color = true;
-              end-of-line-diagnostics = "hint";
-              popup-border = "all";
-              cursor-shape = {
-                insert = "bar";
-                select = "underline";
-              };
-              file-picker.hidden = false;
-              lsp = {
-                display-inlay-hints = true;
-                display-messages = true;
-                display-progress-messages = true;
-                goto-reference-include-declaration = false;
-              };
-              inline-diagnostics.cursor-line = "info";
-              soft-wrap.enable = true;
-            };
-
-            keys = let
-              common = {
-                X = "extend_line_above";
-                space = {
-                  q = ":quit";
-                  Q = ":quit!";
-                  w = ":write";
-                  W = ":write!";
-                  x = ":bc!";
-                  "C-r" = ":rla";
+              editor = {
+                bufferline = "always";
+                cursorline = true;
+                color-modes = true;
+                true-color = true;
+                end-of-line-diagnostics = "hint";
+                popup-border = "all";
+                cursor-shape = {
+                  insert = "bar";
+                  select = "underline";
                 };
-              };
-            in {
-              normal =
-                common
-                // {
-                  "C-l" = "goto_next_buffer";
-                  "C-h" = "goto_previous_buffer";
-                  "C-x" = ":buffer-close";
+                file-picker.hidden = false;
+                lsp = {
+                  display-inlay-hints = true;
+                  display-messages = true;
+                  display-progress-messages = true;
+                  goto-reference-include-declaration = false;
                 };
-              select = common;
-            };
-          };
-      };
-
-      "helix/languages.toml".value = tomlFormat.generate "sumi-helix-languages.toml" {
-        language = [
-          {
-            name = "rust";
-            language-servers = ["rust-analyzer"];
-          }
-          {
-            name = "markdown";
-            language-servers = ["marksman"];
-          }
-          {
-            name = "nix";
-            language-servers = [
-              "nil"
-              "nixd"
-            ];
-            formatter = {
-              command = "alejandra";
-              args = ["-"];
-            };
-            auto-format = true;
-          }
-          {
-            name = "dart";
-            language-servers = ["dart"];
-          }
-          {
-            name = "toml";
-            language-servers = ["taplo"];
-            formatter = {
-              command = "taplo";
-              args = [
-                "fmt"
-                "-"
-              ];
-            };
-            auto-format = true;
-          }
-        ];
-
-        language-server = {
-          rust-analyzer = {
-            command = "rust-analyzer";
-            config = {
-              checkOnSave.enable = true;
-              inlayHints = {
-                chainingHints.enable = true;
-                typeHints.enable = false;
-                parameterHints.enable = false;
+                inline-diagnostics.cursor-line = "info";
+                soft-wrap.enable = true;
               };
-              procMacro.enable = true;
+
+              keys = let
+                common = {
+                  X = "extend_line_above";
+                  space = {
+                    q = ":quit";
+                    Q = ":quit!";
+                    w = ":write";
+                    W = ":write!";
+                    x = ":bc!";
+                    "C-r" = ":rla";
+                  };
+                };
+              in {
+                normal =
+                  common
+                  // {
+                    "C-l" = "goto_next_buffer";
+                    "C-h" = "goto_previous_buffer";
+                    "C-x" = ":buffer-close";
+                  };
+                select = common;
+              };
             };
-          };
-
-          nixd = {
-            command = "nixd";
-            args = ["--semantic-tokens=true"];
-          };
-
-          dart.command = "dart";
         };
-      };
 
-      "helix/plugins".value = config.lib.sumi.mkOutOfStoreSymlink "${config.lib.sumi.paths.flakeRootOrErr}/modules/helix/plugins";
+        "helix/languages.toml".value = tomlFormat.generate "sumi-helix-languages.toml" {
+          language = [
+            {
+              name = "rust";
+              language-servers = ["rust-analyzer"];
+            }
+            {
+              name = "markdown";
+              language-servers = ["marksman"];
+            }
+            {
+              name = "nix";
+              language-servers = [
+                "nil"
+                "nixd"
+              ];
+              formatter = {
+                command = "alejandra";
+                args = ["-"];
+              };
+              auto-format = true;
+            }
+            {
+              name = "dart";
+              language-servers = ["dart"];
+            }
+            {
+              name = "toml";
+              language-servers = ["taplo"];
+              formatter = {
+                command = "taplo";
+                args = [
+                  "fmt"
+                  "-"
+                ];
+              };
+              auto-format = true;
+            }
+          ];
 
-      "helix/themes/modus.toml".value = ./themes/modus.toml;
-      "helix/themes/melissa-light.toml".value = ./themes/melissa-light.toml;
-      "helix/themes/space-age.toml".value = ./themes/space-age.toml;
-      "helix/themes/gruvbox.toml".value = ./themes/gruvbox.toml;
-      "helix/themes/ashen.toml".value = ./themes/ashen.toml;
-    };
+          language-server = {
+            rust-analyzer = {
+              command = "rust-analyzer";
+              config = {
+                checkOnSave.enable = true;
+                inlayHints = {
+                  chainingHints.enable = true;
+                  typeHints.enable = false;
+                  parameterHints.enable = false;
+                };
+                procMacro.enable = true;
+              };
+            };
+
+            nixd = {
+              command = "nixd";
+              args = ["--semantic-tokens=true"];
+            };
+
+            dart.command = "dart";
+          };
+        };
+
+      "helix/plugins".value = config.lib.sumi.mkOutOfStoreSymlink "${helixRoot}/plugins";
+    }
+      // builtins.listToAttrs
+      (map (name: {
+          name = "helix/themes/${name}.toml";
+          value.value = config.lib.sumi.mkOutOfStoreSymlink "${helixRoot}/themes/${name}.toml";
+        }) [
+          "modus"
+          "melissa-light"
+          "space-age"
+          "gruvbox"
+          "ashen"
+        ]);
 
     sumi.hook.helix = {
       watch = ["theme"];
