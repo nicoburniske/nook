2f7fe8a8e9a213824a57db09a65d0ea5cec724c0
Nico Burniske <nicoburniske@gmail.com>
2026-04-18 11:02:35 -0400

    update nixpkgs and codex

--- hosts/snowflake/configuration.nix
+++ hosts/snowflake/configuration.nix
@@ -34,6 +34,15 @@ in {
   nixpkgs.overlays = [
     (final: prev: {
       ungoogled-chromium = prev.ungoogled-chromium.override {enableWideVine = true;};
+      codex = prev.codex.overrideAttrs (old: {
+        auditable = false;
+        postPatch =
+          (old.postPatch or "")
+          + ''
+            substituteInPlace Cargo.toml \
+              --replace-fail 'lto = "fat"' 'lto = "thin"'
+          '';
+      });
     })
   ];
 
