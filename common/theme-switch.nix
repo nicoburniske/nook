4713446a91ae93791d2845fe5563fe3494a282b6
Nico Burniske <nicoburniske@gmail.com>
2026-05-01 12:34:46 -0400

    theme-switch: cursor start on current theme

--- common/theme-switch.nix
+++ common/theme-switch.nix
@@ -6,9 +6,10 @@
       exit 0
     }
 
+    let theme_data = ($themes_result.stdout | from json)
+    let current_theme = ($theme_data | get current)
     let themes = (
-      $themes_result.stdout
-      | from json
+      $theme_data
       | get variants
       | str join "\n"
       | str trim
@@ -29,31 +30,31 @@
     }
   '';
 in
-if pkgs.stdenv.isDarwin
-then
-  pkgs.writeNuScriptBin "theme-switch" ''
-    let kitten = "${pkgs.kitty}/bin/kitten"
-    let nu_bin = "${pkgs.nushell}/bin/nu"
+  if pkgs.stdenv.isDarwin
+  then
+    pkgs.writeNuScriptBin "theme-switch" ''
+      let kitten = "${pkgs.kitty}/bin/kitten"
+      let nu_bin = "${pkgs.nushell}/bin/nu"
 
-    ^$kitten quick-access-terminal --instance-group theme-selector $nu_bin -c '
-      let fzf = "${pkgs.fzf}/bin/fzf"
+      ^$kitten quick-access-terminal --instance-group theme-selector $nu_bin -c '
+        let fzf = "${pkgs.fzf}/bin/fzf"
+        ${loadThemes}
+
+        let pick = (do {
+          $themes | ^$fzf --prompt "Select theme: " --layout reverse --border rounded --color dark
+        } | complete)
+
+        ${applySelection}
+      '
+    ''
+  else
+    pkgs.writeNuScriptBin "theme-switch" ''
+      let fuzzel = "${pkgs.fuzzel}/bin/fuzzel"
       ${loadThemes}
 
       let pick = (do {
-        $themes | ^$fzf --prompt "Select theme: " --layout reverse --border rounded --color dark
+        $themes | ^$fuzzel --dmenu --prompt "theme> " $"--select=($current_theme)"
       } | complete)
 
       ${applySelection}
-    '
-  ''
-else
-  pkgs.writeNuScriptBin "theme-switch" ''
-    let fuzzel = "${pkgs.fuzzel}/bin/fuzzel"
-    ${loadThemes}
-
-    let pick = (do {
-      $themes | ^$fuzzel --dmenu --prompt "theme> "
-    } | complete)
-
-    ${applySelection}
-  ''
+    ''
