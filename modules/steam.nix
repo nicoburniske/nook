fbd92014525479321174308d7d2779a99b9b0ff8
Nico Burniske <nicoburniske@gmail.com>
2026-06-30 18:28:10 -0400

    steam: remove pipewire restart

--- modules/steam.nix
+++ modules/steam.nix
@@ -3,21 +3,13 @@
     host,
     pkgs,
     ...
-  }: let
-    # hack for startup crash
-    # https://github.com/NixOS/nixpkgs/issues/324875#issuecomment-2308355036
-    steam = pkgs.steam.override (prev: {
-      extraPreBwrapCmds =
-        (prev.extraPreBwrapCmds or "")
-        + ''
-          ${pkgs.systemd}/bin/systemctl --user restart pipewire || true
-        '';
-    });
-  in {
+  }:
+  # https://github.com/NixOS/nixpkgs/issues/324875#issuecomment-2308355036
+  # systemctl --user restart pipewire
+  {
     programs.steam = {
       enable = true;
       extest.enable = true;
-      package = steam;
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       extraPackages = [
