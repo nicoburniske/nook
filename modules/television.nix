42c420bdcf8c83365c8ab22a8b7be1a632420fed
Nico Burniske <nicoburniske@gmail.com>
2026-07-01 21:15:15 -0400

    television: remove channels

--- modules/television.nix
+++ modules/television.nix
@@ -3,38 +3,6 @@
     environment.systemPackages = [pkgs.television];
 
     sumi.configFile = {
-      "television/cable/files.toml".value = ''
-        [metadata]
-        name = "files"
-        description = "A channel to select files and directories"
-        requirements = ["fd", "bat"]
-
-        [source]
-        command = "fd -t f"
-
-        [preview]
-        command = "bat -n --color=always '{}'"
-      '';
-
-      "television/cable/text.toml".value = ''
-        [metadata]
-        name = "text"
-        description = "A channel to find and select text from files"
-        requirements = [ "rg", "bat",]
-
-        [source]
-        command = "rg . --no-heading --line-number --colors 'match:fg:white' --colors 'path:fg:blue' --color=always --smart-case"
-        ansi = true
-        output = "{strip_ansi|split:\\::..2}"
-
-        [preview]
-        command = "bat -n --color=always '{strip_ansi|split:\\::0}'"
-        offset = "{strip_ansi|split:\\::1}"
-
-        [ui.preview_panel]
-        header = "{strip_ansi|split:\\::..2}"
-      '';
-
       "television/cable/just.toml".value = ''
         [metadata]
         name = "just"
