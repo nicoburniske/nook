6eeecd9a34a7a9ad6a9d494c2a79c682f447c798
Nico Burniske <nicoburniske@gmail.com>
2026-08-12 18:02:18 -0400

    update scd

--- modules/scd/mod.nix
+++ modules/scd/mod.nix
@@ -6,39 +6,41 @@
 
   nixosModules.scd = {
     host,
+    lib,
     pkgs,
     ...
   }: {
     imports = [inputs.scd.nixosModules.default];
-
     services.scd = {
       enable = true;
-      configFile = ./config.toml;
+      configFile = ./config.kdl;
     };
     users.users.${host.user}.extraGroups = ["scd-control"];
-
     sumi = {
-      configFile."scd/osk.toml" = {
+      configFile."scd/osk.kdl" = {
         watch = "theme";
         value = ctx:
-          (pkgs.formats.toml {}).generate "scd-osk.toml" {
-            font = ctx.value.fonts.sansSerif.file;
-            colors = with ctx.value.colors; {
-              background = "#${base00}";
-              border = "#${base03}";
-              key = "#${base01}";
-              special = "#${base02}";
-              hover = "#${base05}";
-              pressed = "#${base0D}";
-              pressed_foreground = "#${base00}";
-              foreground = "#${base05}";
-              muted = "#${base04}";
-              dim = "#${base03}";
-              hint_paddle = "#${base02}";
-              hint_control = "#${base01}";
-              shadow = "#000000";
-            };
-          };
+          with ctx.value.colors;
+            lib.kdl.toKDL [
+              {font = ctx.value.fonts.sansSerif.file;}
+              {
+                colors = {
+                  background = "#${base00}";
+                  border = "#${base03}";
+                  key = "#${base01}";
+                  special = "#${base02}";
+                  hover = "#${base05}";
+                  pressed = "#${base0D}";
+                  pressed-foreground = "#${base00}";
+                  foreground = "#${base05}";
+                  muted = "#${base04}";
+                  dim = "#${base03}";
+                  hint-paddle = "#${base02}";
+                  hint-control = "#${base01}";
+                  shadow = "#000000";
+                };
+              }
+            ];
       };
       hook.scd-osk = {
         watch = "theme";
