f596ab119a9e4ae00218b6d9b8d7f4d5d71b8786
Nico Burniske <nicoburniske@gmail.com>
2026-08-14 11:49:37 -0400

    sunshine

--- modules/sunshine.mod.nix
+++ modules/sunshine.mod.nix
@@ -1,9 +1,19 @@
 {
-  nixosModules.sunshine = {...}: {
+  nixosModules.sunshine = {pkgs, ...}: {
     services.sunshine = {
       enable = true;
       autoStart = true;
       capSysAdmin = true;
+      package = pkgs.sunshine.overrideAttrs (oldAttrs: {
+        patches =
+          (oldAttrs.patches or [])
+          ++ [
+            (pkgs.fetchurl {
+              url = "https://github.com/LizardByte/Sunshine/commit/86a253859e2eac62f41a2821804efa90ae8215c8.patch";
+              hash = "sha256-hicZPJ0dHeiN10sWLeIP36VcQSZW5xAPnktrPq+8REQ=";
+            })
+          ];
+      });
       settings.origin_web_ui_allowed = "wan";
     };
 
