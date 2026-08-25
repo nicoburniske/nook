fb2726bb25e1aa7937b4fad6837e374571633b41
Nico Burniske <nicoburniske@gmail.com>
2026-08-24 20:21:17 -0400

    coolerconrtol: port to AIO

--- modules/coolercontrol.mod.nix
+++ modules/coolercontrol.mod.nix
@@ -5,32 +5,16 @@
     ...
   }: let
     profileUid = name: builtins.hashString "sha256" "coolercontrol-profile:${name}";
-    profile = {
-      case = profileUid "quiet case";
-      caseCpu = profileUid "quiet case cpu";
-      caseGpu = profileUid "quiet case gpu";
-      cpu = profileUid "quiet cpu";
-    };
+    profile.case = profileUid "quiet case";
     nct6799Uid = "00a4da18625f56275c89e2fcd25a83c08c5ad3326452fa7e252fcc8a89c92493";
     cpuUid = "9378f45b621719636560170d30c952878cd910cc15c883d07647dd9f96577a54";
     gpuUid = "97910386cac9bfce54b2c224e4aaef42cd953440cb57f1ff5ff46ac183bf338e";
+    customSensorsUid = "19e098e312e1b1b39163a343ea22b6ea17f18ec1a803ffe0ce44f5bacd6076ee";
     inherit (lib.toml) inlineTable;
 
     quietCaseProfile = {
       uid = profile.case;
       name = "quiet case";
-      p_type = "Mix";
-      function_uid = "0";
-      member_profile_uids = [
-        profile.caseCpu
-        profile.caseGpu
-      ];
-      mix_function_type = "Avg";
-    };
-
-    quietCaseCpuProfile = {
-      uid = profile.caseCpu;
-      name = "quiet case cpu";
       p_type = "Graph";
       function_uid = "0";
       member_profile_uids = [];
@@ -46,53 +30,8 @@
       temp_min = 0.0;
       temp_max = 100.0;
       temp_source = inlineTable {
-        device_uid = cpuUid;
-        temp_name = "temp1";
-      };
-    };
-
-    quietCaseGpuProfile = {
-      uid = profile.caseGpu;
-      name = "quiet case gpu";
-      p_type = "Graph";
-      function_uid = "0";
-      member_profile_uids = [];
-      offset_profile = [];
-      speed_profile = [
-        [30.0 15]
-        [50.0 25]
-        [65.0 40]
-        [80.0 65]
-        [95.0 100]
-      ];
-      temp_min = 30.0;
-      temp_max = 95.0;
-      temp_source = inlineTable {
-        device_uid = gpuUid;
-        temp_name = "temp1";
-      };
-    };
-
-    quietCpuProfile = {
-      uid = profile.cpu;
-      name = "quiet cpu";
-      p_type = "Graph";
-      function_uid = "0";
-      member_profile_uids = [];
-      offset_profile = [];
-      speed_profile = [
-        [30.0 22]
-        [60.0 34]
-        [75.0 50]
-        [85.0 78]
-        [90.0 90]
-        [100.0 100]
-      ];
-      temp_min = 30.0;
-      temp_max = 100.0;
-      temp_source = inlineTable {
-        device_uid = cpuUid;
-        temp_name = "temp1";
+        device_uid = customSensorsUid;
+        temp_name = "case_temp";
       };
     };
 
@@ -101,14 +40,38 @@
         ${nct6799Uid} = "nct6799";
         ${cpuUid} = "AMD Ryzen 9 7900 12-Core Processor";
         ${gpuUid} = "Navi 48 [Radeon RX 9070/9070 XT/9070 GRE]";
+        ${customSensorsUid} = "Custom Sensors";
       };
+
+      custom_sensors = [
+        {
+          id = "case_temp";
+          cs_type = "Mix";
+          mix_function = "WeightedAvg";
+          sources = [
+            {
+              temp_source = inlineTable {
+                device_uid = cpuUid;
+                temp_name = "temp1";
+              };
+              weight = 75;
+            }
+            {
+              temp_source = inlineTable {
+                device_uid = gpuUid;
+                temp_name = "temp1";
+              };
+              weight = 25;
+            }
+          ];
+        }
+      ];
 
       legacy690 = {};
 
       device-settings.${nct6799Uid} = {
-        fan1 = inlineTable {profile_uid = profile.case;};
-        fan2 = inlineTable {profile_uid = profile.cpu;};
-        fan6 = inlineTable {profile_uid = profile.case;};
+        fan2 = inlineTable {profile_uid = profile.case;};
+        fan7 = inlineTable {speed_fixed = 100;};
       };
 
       profiles = [
@@ -120,9 +83,6 @@
           member_profile_uids = [];
         }
         quietCaseProfile
-        quietCaseCpuProfile
-        quietCaseGpuProfile
-        quietCpuProfile
       ];
 
       functions = [
@@ -135,7 +95,7 @@
 
       settings = {
         apply_on_boot = true;
-        liquidctl_integration = true;
+        liquidctl_integration = false;
         hide_duplicate_devices = true;
         no_init = false;
         startup_delay = 2;
